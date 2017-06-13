4ce28e9ede9aaad8a0908bf88e3a8bb8b40eac07
freedri <admin@freedri.com>
2017-06-14 01:00:26 +0930

    website redesign

--- javascript/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(window).bind('scroll', function () {
-            if ($(window).scrollTop() > ($("header").outerHeight())) {
-                $('#topbar').addClass('fixed');
-            } else {
-                $('#topbar').removeClass('fixed');
-            }
-        });
-});
